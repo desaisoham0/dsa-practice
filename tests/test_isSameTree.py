4e2f1ddef4058852fa00ae95b8a4331075572e56
Soham Desai <desaisoham0@gmail.com>
2025-08-19 17:45:11 -0400

    feat: add isSubtree solution and tests

--- tests/test_isSameTree.py
+++ tests/test_isSameTree.py
@@ -11,29 +11,31 @@ def build_tree(values):
 	root = kids.pop()
 	for node in nodes:
 		if node:
-			if kids: node.left = kids.pop()
-			if kids: node.right = kids.pop()
+			if kids:
+				node.left = kids.pop()
+			if kids:
+				node.right = kids.pop()
 	return root
 
 def test_same_tree_identical():
 	p = build_tree([1,2,3])
 	q = build_tree([1,2,3])
-	assert Solution().isSameTree(p, q) == True
+	assert Solution().isSameTree(p, q) is True
 
 def test_same_tree_different_structure():
     p = build_tree([1,2])
     q = build_tree([1,None,2])
-    assert Solution().isSameTree(p, q) == False
+    assert Solution().isSameTree(p, q) is False
 
 def test_same_tree_different_values():
     p = build_tree([1,2,1])
     q = build_tree([1,1,2])
-    assert Solution().isSameTree(p, q) == False
+    assert Solution().isSameTree(p, q) is False
 
 def test_same_tree_both_empty():
-    assert Solution().isSameTree(None, None) == True
+    assert Solution().isSameTree(None, None) is True
 
 def test_same_tree_one_empty():
     p = build_tree([1])
     q = None
-    assert Solution().isSameTree(p, q) == False
+    assert Solution().isSameTree(p, q) is False
