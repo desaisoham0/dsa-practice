e86a443c4fd9156ab1c43c115fac92d45977cf28
Soham Desai <desaisoham0@gmail.com>
2025-08-20 16:57:58 -0400

    feat: add check all palindrome problem

---
+++ python/check_all_palindrome.py
@@ -0,0 +1,17 @@
+def reverse_word(word):
+	reversed = ""
+	for letter in word:
+		reversed = letter + reversed
+	return reversed
+
+def is_palindrome(word):
+	return word == reverse_word(word)
+
+def check_all_palindrome(arr):
+	for word in arr:
+		if is_palindrome(word) == False:
+			return False
+
+	return True
+
+print(check_all_palindrome(["racecar", "shoe", "civic"]))
